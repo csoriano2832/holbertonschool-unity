8572125ab63ab9e7dcb184cf25844f659aaa3313
Christopher Soriano <2832@holbertonschool.com>
2022-05-04 01:18:09 -0400

    add intro animations to level 2 and 3

--- 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CutsceneController : MonoBehaviour
 {
     public GameObject mainCamera;
     public GameObject player;
     public GameObject timerCanvas;
+    public Animator anim;
+    private Scene scene;
+
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+        if (scene.name == "Level01") {
+            anim.SetTrigger("Lvl1");
+        }
+        else if (scene.name == "Level02") {
+            anim.SetTrigger("Lvl2");
+        }
+        else {
+            anim.SetTrigger("Lvl3");
+        }
+    }
 
     public void CutsceneCameraEvent()
     {
